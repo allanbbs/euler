11dd9ed8c98ad64cf8bfe7465cd2274f386c6877
allanbbs <>
2025-04-11 23:16:47 +0100

    13th problem

--- utils.hs
+++ utils.hs
@@ -8,3 +8,8 @@ isPrime 2 = True
 isPrime n
     | even n = False
     | otherwise = null [d | d <- [3,5..floor (sqrt (fromIntegral n))], mod n d == 0]
+
+reduce :: Int ->Int -> Int
+reduce n factor
+    | mod n factor == 0 = reduce (div n factor) factor
+    | otherwise = n
