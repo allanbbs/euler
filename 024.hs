baf333895384a067251675a9564f20d967874bec
allanbbs <>
2025-04-17 18:05:41 +0100

    24th problem

--- 024.hs
+++ 024.hs
@@ -1,12 +1,4 @@
 import Data.List (sort)
-perms :: [Char] -> [[Char]]
-perms [] = [[]]
-perms (x:xs) =  [pre ++ x:post | p <- perms xs, (pre,post) <- splits p]
-
-
-splits :: [Char] -> [([Char],[Char])]
-splits [] = [([],[])]
-splits (x:xs) = ([],x:xs) : [(x:pre,post) | (pre,post) <- splits xs]
+import EulerUtils (perms)
 
 main = print ((sort (perms ['1','2','0','3','4','5','6','7','8','9']))!!999999)
-
